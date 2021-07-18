214562bcd4689b0167e7229f77e44e6db9aec3ee
001Abhishek <aprajput321@gmail.com>
2021-07-18 22:06:03 +0530

    adds components folder with js files for frontend

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,16 @@
+
+import React , {Component} from 'react';
+
+export default class CreateExercise extends Component{
+
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                <p>In Create Exercise Component!</p>
+            </div>
+        )
+    }
+}
